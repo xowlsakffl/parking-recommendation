5de4db26eb40b2afbd52f4e33d74fb3c0d65c46f
msan <50791439+xowlsakffl@users.noreply.github.com>
2025-02-05 04:43:55 +0900

    초기 설정

---
+++ src/main/java/dev/be/parkingmap/ParkingMapApplication.java
@@ -0,0 +1,13 @@
+package dev.be.parkingmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/be/parkingmap/ParkingMapApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.parkingmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
